6eaa01a0a9c1d5e7883b72194ee9871f019d2096
usrlab24n <usrlab24n@academico1.eniac>
2026-03-31 16:23:53 -0300

    Criação do Menu Sanduíche

--- frontend/js/eventos.js
+++ frontend/js/eventos.js
@@ -1,3 +1,26 @@
+// Aguarda o carregamento completo do HTML para rodar o script
+document.addEventListener("DOMContentLoaded", function() {
+  const menuSanduiche = document.querySelector('.menu-sanduiche');
+  const navLinks = document.querySelector('.links');
+
+
+  if (menuSanduiche && navLinks) {
+    menuSanduiche.addEventListener('click', () => {
+      // Liga e desliga a classe 'ativo' nos links
+      navLinks.classList.toggle('ativo');
+    });
+  } else {
+    console.error("Erro: Não encontrei o menu ou os links no HTML.");
+  }
+});
+
+
+
+
+
+
+
+
 const btnNext = document.getElementById('nextSlide')
    const btnPrevious = document.getElementById('previousSlide')
    const slider = document.querySelector('.slider')
--- frontend/js/painelADMScript.js
+++ frontend/js/painelADMScript.js
@@ -1,3 +1,23 @@
+// Aguarda o carregamento completo do HTML para rodar o script
+document.addEventListener("DOMContentLoaded", function() {
+    const menuSanduiche = document.querySelector('.menu-sanduiche');
+    const navLinks = document.querySelector('.links');
+  
+  
+    if (menuSanduiche && navLinks) {
+      menuSanduiche.addEventListener('click', () => {
+        // Liga e desliga a classe 'ativo' nos links
+        navLinks.classList.toggle('ativo');
+      });
+    } else {
+      console.error("Erro: Não encontrei o menu ou os links no HTML.");
+    }
+  });
+  
+  
+
+
+
 // --- FUNÇÃO PARA CARREGAR DENÚNCIAS ---
 async function carregarTabelaDenunciasPainel(cabecalho, corpo) {
     // Monta o cabeçalho específico para denúncias
